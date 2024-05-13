c066a17f83b14c20a14e18fc2593a81dcfba2751
Rohitsonvane88 <99544328+Rohitsonvane88@users.noreply.github.com>
2024-05-13 22:40:50 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb+srv://dbUser:dbUserPassword@cluster0.zt5rirz.mongodb.net/?retryWrites=true&w=majority"
+const mongoURI = "mongodb+srv://sonvanerohit88:dbUserPassword@cluster0.c7cwjgp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 mongoose.set('strictQuery', true);
 
 const connectToMongo = () => {
